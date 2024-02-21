fd6696a9c6d00d8b1481d9a9b696ccce9886ae4b
Yangyang Li <yangyang.li@northwestern.edu>
2024-02-21 09:45:29 -0600

    feat: Add KmerPreTokenizer and KmerDecoder classes

--- deepchopper/models/tokenizers.py
+++ deepchopper/models/tokenizers.py
@@ -1,21 +1,6 @@
+from tokenizers import NormalizedString, PreTokenizedString
+
 import deepchopper
-
-
-class KmerPreTokenizer:
-    def __init__(self, kmer_size: int, *, overlap: bool):
-        self.kmer_size = kmer_size
-        self.overlap = overlap
-
-    def pre_tokenize_str(self, sequence: str) -> list[tuple[str, tuple[int, int]]]:
-        """Pre-tokenize a sequence into overlapping kmers.
-
-        Example:
-            sequence = "ATCGG"
-            kmer_size = 3
-            overlap = False
-            pre_tokenize_str(sequence) -> [("ATC", (0, 3)), ("TCG", (1, 4)), ("CGG", (2, 5))]
-        """
-        return deepchopper.seq_to_kmers_and_offset(sequence, self.kmer_size, self.overlap)
 
 
 def compute_tokens_to_ids(kmer_size: int) -> tuple[dict[str, int], list[str]]:
@@ -32,3 +17,26 @@ def compute_tokens_to_ids(kmer_size: int) -> tuple[dict[str, int], list[str]]:
     all_tokens = standard_tokens + specical_tokens
     tokens_to_ids = {tok: i for i, tok in enumerate(all_tokens)}
     return tokens_to_ids, all_tokens
+
+
+class KmerPreTokenizer:
+    def __init__(self, kmer_size: int, *, overlap: bool):
+        self.kmer_size = kmer_size
+        self.overlap = overlap
+
+    def kmer_split(self, i: int, normalized_string: NormalizedString) -> list[NormalizedString]:
+        return [
+            normalized_string[start:end]
+            for (_token, (start, end)) in deepchopper.seq_to_kmers_and_offset(
+                sequence, self.kmer_size, self.overlap
+            )
+        ]
+
+    def pre_tokenize(self, pretok: PreTokenizedString):
+        # Let's call split on the PreTokenizedString to split using `self.jieba_split`
+        pretok.split(self.kmer_split)
+
+
+class KmerDecoder:
+    def decode(self, tokens: List[str]) -> str:
+        return "".join(tokens)
