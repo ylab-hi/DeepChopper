e0a8c494ef5cb22b34f6afe3cfe712aa6f3d9b68
Yangyang Li <yangyang.li@northwestern.edu>
2024-03-03 21:10:48 -0600

    feat: Update training script with new dataset and options

--- train.sh
+++ train.sh
@@ -3,21 +3,24 @@ set -e
 set -u
 set -o pipefail
 
-dc-hg-train --hyenadna_model hyenadna-small-32k-seqlen \
-	--dataset_name tests/data/test_input.parquet \
+# --dataset_name tests/data/test_input.parquet
+dc-hg-train \
+	--hyenadna_model hyenadna-small-32k-seqlen \
+	--train_file data/train.parquet \
+	--validation_file data/val.parquet \
+	--test_file data/test.parquet \
+	--output_dir notebooks/hyena_model_train \
+	--num_train_epochs 1 \
 	--learning_rate 2e-5 \
 	--per_device_train_batch_size 8 \
 	--per_device_eval_batch_size 8 \
 	--weight_decay 0.01 \
 	--torch_compile False \
 	--push_to_hub False \
-	--num_train_epochs 1 \
-	--weight_decay 0.01 \
-	--output_dir notebooks/hyena_model_train \
 	--evaluation_strategy epoch \
 	--save_strategy epoch \
 	--load_best_model_at_end \
 	--trust_remote_code \
 	--report_to wandb \
-	--run_name deepchopper \
+	--run_name deepchopper_train \
 	--do_train --do_eval --do_predict
