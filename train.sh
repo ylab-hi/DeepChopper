a7effb48e7ce2d2edb65cf43d56b7db96dc0d7d8
Yangyang Li <yangyang.li@northwestern.edu>
2024-03-05 16:06:01 -0600

    feat: Update training script for 600000 samples and 15 epochs

--- train.sh
+++ train.sh
@@ -8,17 +8,17 @@ set -o pipefail
 # --max_eval_samples 100 \
 # --max_predict_samples 100 \
 # 24 batch size for 60GB GPU
-
 dc-hg-train \
 	--hyenadna_model hyenadna-small-32k-seqlen \
-	--train_file data/train.parquet \
-	--validation_file data/val.parquet \
-	--test_file data/test.parquet \
-	--output_dir notebooks/deepchopper_train \
-	--num_train_epochs 150 \
+	--train_file data/600000_samples/train.parquet \
+	--validation_file data/600000_samples/val.parquet \
+	--test_file data/600000_samples/test.parquet \
+	--output_dir notebooks/dc_train_600000_15_ep_18b \
+	--num_train_epochs 15 \
 	--learning_rate 2e-5 \
-	--per_device_train_batch_size 24 \
-	--per_device_eval_batch_size 24 \
+	--gradient_accumulation_steps 4 \
+	--per_device_train_batch_size 18 \
+	--per_device_eval_batch_size 18 \
 	--weight_decay 0.01 \
 	--torch_compile False \
 	--push_to_hub False \
@@ -27,5 +27,5 @@ dc-hg-train \
 	--load_best_model_at_end \
 	--trust_remote_code \
 	--report_to wandb \
-	--run_name deepchopper_train_200 \
+	--run_name dc_train_600000_15_ep_18b \
 	--do_train --do_eval --do_predict
