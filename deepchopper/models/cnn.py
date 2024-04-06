40b541e81ed6b005a0e2f00a032b822be9fffc7b
Yangyang Li <yangyang.li@northwestern.edu>
2024-04-06 16:02:41 -0500

    refactor: Update CNN model architecture and forward method

--- deepchopper/models/cnn.py
+++ deepchopper/models/cnn.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn.functional as F  # noqa: N812
 from torch import nn
 
@@ -15,37 +14,38 @@ class BenchmarkCNN(nn.Module):
         super().__init__()
         self.number_of_classes = number_of_classes
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.convs = nn.ModuleList()
-        self.batch_norms = nn.ModuleList()
 
-        for fs in filter_sizes:
-            self.convs.append(
+        self.qual_linear1 = nn.Sequential(
+            nn.Linear(1, embedding_dim),
+        )
+
+        layers = []
+        in_channels = embedding_dim
+        for idx, fs in enumerate(filter_sizes):
+            layers.append(
                 nn.Conv1d(
-                    in_channels=embedding_dim,
-                    out_channels=num_filters,
+                    in_channels=in_channels,
+                    out_channels=num_filters[idx],
                     kernel_size=fs,
                     padding="same",
                 )
             )
-            self.batch_norms.append(nn.BatchNorm1d(num_filters))
+            layers.append(nn.BatchNorm1d(num_filters[idx]))
+            layers.append(nn.ReLU())
+            in_channels = num_filters[idx]
 
-            # Final convolutional layer to get the number_of_classes predictions per token
-        self.final_conv = nn.Conv1d(
-            in_channels=num_filters * len(filter_sizes),
-            out_channels=number_of_classes,
-            kernel_size=1,
-        )  # this layer doesn't alter sequence length
+        self.model = nn.Sequential(*layers)
 
         # use number of kernel same as the length of the sequence and average pooling
         # then flatten and use dense layers
         self.dense_model = nn.Sequential(
-            nn.Linear(num_filters * len(filter_sizes), number_of_classes),
+            nn.Linear(in_channels, number_of_classes),
         )
 
-    def forward(self, x, input_quals=None):  # Adding `state` to be consistent with other models
-        x = self.embeddings(x)
+    def forward(self, input_ids, input_quals):  # Adding `state` to be consistent with other models
+        x = self.embeddings(input_ids)
+        x = F.relu(x + self.qual_linear1(input_quals.unsqueeze(-1)))
         x = x.transpose(1, 2)  # [batch_size, embedding_dim, input_len]
-        x = [F.relu(bn(conv(x))) for conv, bn in zip(self.convs, self.batch_norms, strict=True)]
-        x = torch.cat(x, dim=1) if len(x) > 1 else x[0]
-        x = self.final_conv(x)
-        return x.transpose(1, 2)  # [batch_size, input_len, num_filters]
+        x = self.model(x)
+        x = x.transpose(1, 2)
+        return self.dense_model(x)  # [batch_size, input_len, num_filters]
