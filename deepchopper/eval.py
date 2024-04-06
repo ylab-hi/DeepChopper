b5d6a437bf8466348c72b79359cc748a31ffeab8
Yangyang Li <yangyang.li@northwestern.edu>
2024-04-06 00:44:45 -0500

    refactor: Update evaluation config and model in deepchopper

--- deepchopper/eval.py
+++ deepchopper/eval.py
@@ -4,22 +4,6 @@ from typing import TYPE_CHECKING, Any
 import hydra
 from omegaconf import DictConfig
 
-# ------------------------------------------------------------------------------------ #
-# the setup_root above is equivalent to:
-# - adding project root dir to PYTHONPATH
-#       (so you don't need to force user to install project as a package)
-#       (necessary before importing any local modules e.g. `from src import utils`)
-# - setting up PROJECT_ROOT environment variable
-#       (which is used as a base for paths in "configs/paths/default.yaml")
-#       (this way all filepaths are the same no matter where you run the code)
-# - loading environment variables from ".env" in root dir
-#
-# you can remove it if you:
-# 1. either install project as a package or move entry files to project root dir
-# 2. set `root_dir` to "." in "configs/paths/default.yaml"
-#
-# more info: https://github.com/ashleve/rootutils
-# ------------------------------------------------------------------------------------ #
 from .utils import (
     RankedLogger,
     extras,
