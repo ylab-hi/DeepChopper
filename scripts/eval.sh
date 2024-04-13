9dcf9eee6bad22580bb53dfc2d85962f6466e394
Yangyang Li <yangyang.li@northwestern.edu>
2024-04-12 19:03:04 -0500

    add eval scripts

---
+++ scripts/eval.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail # Combines the set commands into one line
+
+# Paths to checkpoint files
+hyena_ckpt_path="/projects/b1171/ylk4626/project/DeepChopper/logs/train/runs/2024-04-08_23-19-20/checkpoints/epoch_005_f1_0.9933.ckpt"
+cnn_ckpt_path="/projects/b1171/ylk4626/project/DeepChopper/logs/train/runs/2024-04-07_12-01-37/checkpoints/epoch_036_f1_0.9914.ckpt"
+
+# Default model selection
+model="hyena"
+data_path="data/eval/real_data/dorado_without_trim_fqs/K562.fastq_chunks/K562.fastq_0.parquet"
+num_workers=31
+batch_size=24
+
+# Set the checkpoint path based on the selected model
+if [ "$model" = "cnn" ]; then
+	ckpt_path="$cnn_ckpt_path"
+elif [ "$model" = "hyena" ]; then
+	ckpt_path="$hyena_ckpt_path"
+else
+	echo "Error: Model not supported."
+	exit 1
+fi
+
+# Evaluate the model
+poe eval \
+	ckpt_path="$ckpt_path" \
+	model="$model" \
+	+data.predict_data_path="$data_path" \
+	trainer=gpu \
+	data.num_workers=$num_workers \
+	data.batch_size=$batch_size
