3c79dc1721000ce198dbdc1185739f0c04c7fad4
Yangyang Li <yangyang.li@northwestern.edu>
2024-05-20 20:48:55 -0500

    feat: Add KanModel class for deep learning model

---
+++ deepchopper/models/kan.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn.functional as F  # noqa: N812
+from torch import nn
+
+
+class KanModel(nn.Module):
+    def __init__(self, input_size, number_of_classes, vocab_size, embedding_dim):
+        super(self).__init__()
+
+    def forward(self, x):
+        pass
