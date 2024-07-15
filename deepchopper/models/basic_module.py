fix: Update precision and recall calculation methods

deepchopper/models/basic_module.py
@@ -4,7 +4,7 @@ import torch
 from lightning import LightningModule
 from torch import nn
 from torchmetrics import MaxMetric, MeanMetric
-from torchmetrics.classification import F1Score, PrecisionRecallCurve
+from torchmetrics.classification import F1Score, Precision, Recall
 
 
 class ContinuousIntervalLoss(nn.Module):
@@ -71,9 +71,11 @@ class TokenClassificationLit(LightningModule):
         self.test_acc = F1Score(
             task="binary", num_classes=net.number_of_classes, ignore_index=self.criterion.ignore_index
         )
-        self.test_prc = PrecisionRecallCurve(
+
+        self.test_precision = Precision(
             task="binary", num_classes=net.number_of_classes, ignore_index=self.criterion.ignore_index
         )
+        self.test_recall = Recall(task="binary", num_classes=net.number_of_classes, ignore_index=self.criterion.ignore)
 
         # for averaging loss across batches
         self.train_loss = MeanMetric()
@@ -178,14 +180,16 @@ class TokenClassificationLit(LightningModule):
         self.test_loss(loss)
         self.test_acc(preds, targets)
 
-        self.test_prc(preds, targets)
+        self.test_precision(preds, targets)
+        self.test_recall(preds, targets)
 
         self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("test/f1", self.test_acc, on_step=False, on_epoch=True, prog_bar=True)
 
     def on_test_epoch_end(self) -> None:
         """Lightning hook that is called when a test epoch ends."""
-        percision, recall, thresholds = self.test_prc.compute()
+        self.log("test/precision", self.test_precision)
+        self.log("test/recall", self.test_recall)
 
     def predict_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
         """Perform a single prediction step on a batch of data from the test set.