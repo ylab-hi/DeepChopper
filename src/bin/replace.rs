4bdb99e1fc22910f619505e28e5c8d07c1f73192
Yangyang Li <yangyang.li@northwestern.edu>
2024-08-19 12:38:13 -0500

    feat: Update candle-core version to 0.6.0, add HashSet usage

--- src/bin/replace.rs
+++ src/bin/replace.rs
@@ -7,16 +7,19 @@ use std::path::{Path, PathBuf};
 use std::{fs::File, num::NonZeroUsize, thread};
 
 use ahash::HashMap;
+use ahash::HashSet;
 use bstr::BString;
-use noodles::sam::alignment::record::data::field::Tag;
-use noodles::sam::alignment::record::data::field::Value;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// path to the bam file
-    #[arg(short, long, value_name = "bam")]
-    bam: Vec<PathBuf>,
+    /// path to the dc bam file
+    #[arg(long)]
+    dcbam: PathBuf,
+
+    #[arg(long)]
+    /// path to the do bam file
+    dobam: PathBuf,
 
     /// threads number
     #[arg(short, long, default_value = "2")]
@@ -46,27 +49,74 @@ fn load_internal_read<P: AsRef<Path>>(
     let mut reader = bam::io::Reader::from(decoder);
     let _header = reader.read_header()?;
 
-    let res: HashMap<Bstring, bam::record::Record> = reader
+    let res: HashMap<BString, bam::record::Record> = reader
         .records()
         .par_bridge()
         .filter_map(|result| {
             let record = result.unwrap();
-            let is_mapped = !record.flags().is_unmapped();
-            let is_not_secondary = !record.flags().is_secondary();
-            let is_primary = !record.flags().is_supplementary();
+            let name = record.name().unwrap().as_bytes().to_vec();
 
-            let name = record.name();
+            if name.contains(&b'I') {
+                Some((name.into(), record))
+            } else {
+                None
+            }
+        })
+        .collect();
 
-            if is_mapped && is_not_secondary && is_primary && has_sa_tag {
+    Ok(res)
+}
+
+fn replace_internal<P: AsRef<Path>>(dc_path: P, do_path: P, threads: Option<usize>) -> Result<()> {
+    let internal_records = load_internal_read(dc_path, threads)?;
+    let internal_records_names: HashSet<BString> = internal_records
+        .keys()
+        .par_bridge()
+        .map(|key| {
+            assert!(key.contains(&b'I'));
+            let name = key.split(|&c| c == b'|').next().unwrap();
+            name.into()
+        })
+        .collect();
+
+    let file = File::open(do_path)?;
+
+    let worker_count = if let Some(threads) = threads {
+        NonZeroUsize::new(threads)
+            .unwrap()
+            .min(thread::available_parallelism().unwrap_or(NonZeroUsize::MIN))
+    } else {
+        thread::available_parallelism().unwrap_or(NonZeroUsize::MIN)
+    };
+
+    let decoder = bgzf::MultithreadedReader::with_worker_count(worker_count, file);
+    let mut reader = bam::io::Reader::from(decoder);
+    let header = reader.read_header()?;
+
+    let mut res: Vec<bam::record::Record> = reader
+        .records()
+        .par_bridge()
+        .filter_map(|result| {
+            let record = result.unwrap();
+            let name: BString = record.name().unwrap().as_bytes().to_vec().into();
+
+            if !internal_records_names.contains(&name) {
                 Some(record)
             } else {
                 None
             }
         })
         .collect();
-}
 
-fn replace_internal<P: AsRef<Path>>(dc_path: P, do_path: P) -> Result<()> {
+    res.extend(internal_records.into_values());
+
+    let stdout = std::io::stdout().lock();
+    let mut writer = bam::io::Writer::new(stdout);
+    writer.write_header(&header)?;
+    for record in res {
+        writer.write_record(&header, &record)?;
+    }
+
     Ok(())
 }
 
@@ -87,6 +137,8 @@ fn main() -> Result<()> {
         .num_threads(cli.threads.unwrap())
         .build_global()
         .unwrap();
+
+    replace_internal(cli.dcbam, cli.dobam, cli.threads)?;
 
     let elapsed = start.elapsed();
     log::info!("elapsed time: {:.2?}", elapsed);
