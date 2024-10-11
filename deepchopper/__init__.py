1e18479bd75d913cf346f19b045356156b409754
Yangyang Li <yangyang.li@northwestern.edu>
2024-10-11 02:03:20 -0500

    chore: Update concurrency configuration and CLI import

--- deepchopper/__init__.py
+++ deepchopper/__init__.py
@@ -1,7 +1,8 @@
 """DeepChopper package."""
 
 from . import data, eval, models, train, ui, utils
+from .cli import app
 from .deepchopper import *  # noqa: F403
 from .models import DeepChopper
 
-__all__ = ["models", "utils", "data", "train", "eval", "DeepChopper", "ui"]
+__all__ = ["models", "utils", "data", "train", "eval", "DeepChopper", "ui", "app"]
