ddac13886dbc3d92cbf2736baa5464aa90971584
Yangyang Li <yangyang.li@northwestern.edu>
2024-11-04 21:11:51 -0600

    build: Update project versions to 1.2.6

--- deepchopper/__init__.py
+++ deepchopper/__init__.py
@@ -4,6 +4,6 @@ from . import cli, data, eval, models, train, ui, utils
 from .deepchopper import *  # noqa: F403
 from .models import DeepChopper
 
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 
 __all__ = ["models", "utils", "data", "train", "eval", "DeepChopper", "ui", "cli", "__version__"]
