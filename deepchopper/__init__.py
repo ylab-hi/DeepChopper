e4de79bfe3c1c79d223a34cc661d6ae1b99ea970
Yangyang Li <yangyang.li@northwestern.edu>
2026-01-20 22:31:16 -0600

    dev: bump minor version

--- deepchopper/__init__.py
+++ deepchopper/__init__.py
@@ -4,6 +4,6 @@ from . import cli, data, eval, models, train, ui, utils
 from .deepchopper import *  # noqa: F403
 from .models import DeepChopper
 
-__version__ = "1.2.9"
+__version__ = "1.3.0"
 
 __all__ = ["DeepChopper", "__version__", "cli", "data", "eval", "models", "train", "ui", "utils"]
